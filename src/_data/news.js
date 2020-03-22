8446457428c60f329aa68a47ba8baf99bd6d6a38
cannero <serunmal@gmail.com>
2020-03-22 13:05:36 +0100

    build localized site, fcc-5

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,12 +1,23 @@
 const axios = require("axios");
+const countries = require("./countries.json");
 require("dotenv").config();
 
-module.exports = async function() {
+module.exports = async function () {
+    const newsPromises = countries.map(getNews);
+    return Promise.all(newsPromises).then(newsObj => {
+        return [].concat.apply([], newsObj)
+    });
+};
+
+async function getNews(country) {
     try {
-      const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWSAPI_KEY}&pageSize=5`);
-      return response.data;
+        const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.NEWSAPI_KEY}&pageSize=5`);
+        return {
+            "country": country,
+            "articles": response.data.articles
+        };
     } catch (error) {
-      console.error(error);
+        console.error(error);
     }
-  
-  };
+
+};
